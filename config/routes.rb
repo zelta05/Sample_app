5bbcf0425fd07c67604e555f920df004597e539d
Wilson De la Cruz <wilsondlc2813@gmail.com>
2019-05-17 14:42:46 +0000

    Finished static page

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+  
+  get  'static_pages/about'
+  
+  get 'static_pages/contact'
+
+
+    resources :users
+   # root 'static_pages#home'
+ 
+end
