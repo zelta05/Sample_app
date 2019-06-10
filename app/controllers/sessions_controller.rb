042672191bc25f37e30d10c85f6e0ec93ca2d1b5
Wilson De la Cruz <wilsondlc2813@gmail.com>
2019-06-10 14:11:55 +0000

    Finish user edit, delete, index and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       
-      redirect_to user
+      redirect_back_or user
 
      # Create an error message
     else
