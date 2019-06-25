fa7a9da03f6b27102480188df10d06409e4a43dc
Wilson De la Cruz <wilsondlc2813@gmail.com>
2019-06-25 19:37:53 +0000

    Test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@ User.create!(name:  "Example User",
             admin: true,
             activated: true,
             activated_at: Time.zone.now)
+
+
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -25,3 +27,12 @@ users = User.order(:created_at).take(6)
   users.each { |user| user.microposts.create!(content: content) }
   
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
+
